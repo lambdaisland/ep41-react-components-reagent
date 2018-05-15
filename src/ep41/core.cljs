ddcf146d5269845d2141b31f98d46337978a5f4d
Arne Brasseur <arne@arnebrasseur.net>
2018-05-15 12:24:28 +0200

    Render a table

--- src/ep41/core.cljs
+++ src/ep41/core.cljs
@@ -1,4 +1,33 @@
 (ns ep41.core
   (:require [reagent.core :as r]))
 
-(r/render [:div "hello, venus"] (js/document.getElementById "app"))
+
+
+(def state (r/atom {:page 1
+                    :page-size 10
+                    :rows []}))
+
+(defn table [rows]
+  [:table
+   [:thead
+    (into [:tr]
+          (for [x ["Title" "Name" "Type" "Origin" "Description"]]
+            [:th x]))]
+   (into [:tbody]
+         (for [row rows]
+           (into [:tr]
+                 (for [cell row]
+                   [:td cell]))))])
+
+(defn app []
+  (let [{:keys [page page-size rows]} @state]
+    [table (->> rows
+                (drop (* (dec page) page-size))
+                (take page-size))]))
+
+(r/render [app] (js/document.getElementById "app"))
+
+(-> (js/fetch "teas.json")
+    (.then #(.json %))
+    (.then (fn [json]
+             (swap! state assoc :rows json))))
